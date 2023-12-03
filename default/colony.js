df08a5c98920094abb0607d3c74cfc7ba1697797
matt maggio <mattmaggio19@gmail.com>
2023-12-03 13:04:33 -0600

    Still starting to get my head around the room stuff.

--- default/colony.js
+++ default/colony.js
@@ -25,7 +25,8 @@ class colony{
     updateData(){      
         this.overallAvailableEnergy = Game.rooms[ this.roomName].energyAvailable;
         this.energyCapacity = Game.rooms[ this.roomName].energyCapacityAvailable;
-        this.harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        // this.harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+        this.harvesters =  
         this.upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         this.builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         this.extensions = _.filter(Game.structures, (structure) => structure.structureType == STRUCTURE_EXTENSION);
@@ -35,6 +36,14 @@ class colony{
 
     makeCreeps(){
     //spawn up to a number of each role
+    if(this.harvesters.length < harvesterCount) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        var result  = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+        {memory: {role: 'harvester'}});
+        console.log('Spawning new harvester: ' + newName  + '  Result: '+ result.toString() ); 
+    }   
+
     if(this.upgraders.length < upgraderCount) {
         var newName = 'Upgrader' + Game.time;
         // console.log('Spawning new Upgrader: ' + newName);
@@ -47,14 +56,7 @@ class colony{
         // console.log('Spawning new Upgrader: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
             {memory: {role: 'builder'}});
-    }
-
-        if(this.harvesters.length < harvesterCount) {
-            var newName = 'Harvester' + Game.time;
-            // console.log('Spawning new harvester: ' + newName);
-            Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});
-        }   
+    }   
 
     }
 
@@ -63,12 +65,11 @@ class colony{
     
         if((totalExtensions) < extensionCount) {
             var newName = 'Extension' + Game.time;
-            var spawnPosition = Game.spawns['Spawn1'].pos;
-    
+            var spawnPosition = Game.spawns['Spawn1'].pos;    
             console.log('______________________________________________________________________');
-            console.log('Finding a new extension construction site: ' + newName);
-            
-            var result = Game.rooms['W5N8'].createConstructionSite(spawnPosition.x - (3 * Math.random()) , spawnPosition.y  - (3* Math.random()), STRUCTURE_EXTENSION, newName);
+            console.log('Finding a new extension construction site: ' + newName);            
+            var result = Game.rooms['W5N8'].createConstructionSite(spawnPosition.x - (3 * Math.random()) 
+                        , spawnPosition.y  - (3* Math.random()), STRUCTURE_EXTENSION, newName);
             console.log('Extension construction site result: ' + result);
         }
     }
