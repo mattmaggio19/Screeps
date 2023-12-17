2e9b7ee4843fdc29a376d48fcacc2b599a46badd
matt maggio <mattmaggio19@gmail.com>
2023-12-17 08:05:01 -0600

    Upgrading the upgrader

--- default/role.builder.js
+++ default/role.builder.js
@@ -3,7 +3,7 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-		
+		console.log(creep);
 	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
             creep.say('🔄 pick up');
--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -2,10 +2,11 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        
 
         if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
+            creep.say('🔄 pick up');
 	    }
 	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
 	        creep.memory.upgrading = true;
@@ -18,10 +19,22 @@ var roleUpgrader = {
             }
         }
         else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+            var targets = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+						structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+				}
+			});
+			if(targets.length > 0) {
+				if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+				}
+			}
+            //harvesting bad.
+            // var sources = creep.room.find(FIND_SOURCES);
+            // if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            //     creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            // }
         }
 	}
 };
